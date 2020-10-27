e49e489ea636d397a315d888760208a619a8a58f
Petre Diana <petre.diana98@yahoo.ro>
2020-10-27 19:29:28 +0200

    seminar_5

---
+++ seminar_5/ConsoleApp1/ConsoleApp1/Persoana.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Persoana
+    {
+        public Persoana(string line)
+        {
+            string[] splittedLine = line.Split(',');
+            
+            Nume = splittedLine[0].Trim();
+            Media = decimal.Parse(splittedLine[1].Trim(), CultureInfo.InvariantCulture);
+        }
+        public string Nume { get; set; }
+        public decimal Media { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0:20} - {1,4:0.00}", Nume, Media);
+        }
+    }
+}
---
+++ seminar_5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             string baseFileName = Path.Combine(
+                Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),
+                "Materiale\\students");
+
+            string dataFileName = baseFileName + ".txt";
+            string wordFilePath = baseFileName + ".docx";
+            string excelFilePath = baseFileName + ".xlsx";
+            string excelTemplateFilePath = baseFileName + "Template.xlsx";
+
+            var students = File.ReadAllLines(dataFileName)
+                            .Select(line => new Persoana(line))
+                            .OrderByDescending(persoana => persoana.Media)
+                            .ThenBy(persoana => persoana.Nume)
+                            .ToList();
+
+            students.ForEach(Console.WriteLine);
+        }
+    }
+}
