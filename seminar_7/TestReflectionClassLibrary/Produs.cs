27b70a241707a5fa6a70574641af112ced3bde06
Petre Diana <petre.diana98@yahoo.ro>
2020-11-17 19:51:46 +0200

    seminar 7

---
+++ seminar_7/TestReflectionClassLibrary/Produs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestReflectionClassLibrary
+{
+    public class Produs
+    {
+        private string _denumire;
+        private float _greutate;
+
+        public Produs()
+        {
+            _denumire = "nedefinita";
+            _greutate = 10;
+        }
+
+        public string Denumire
+        {
+            get => _denumire;
+            set => _denumire = value;
+        }
+
+        public float Greutate
+        {
+            get => _greutate;
+            set => _greutate = value;
+        }
+
+        public string AfiseazaProdus()
+        {
+            return _denumire + ", " + _greutate;
+        }
+    }
+}
