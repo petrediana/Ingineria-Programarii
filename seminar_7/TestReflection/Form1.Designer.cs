7ea224c737a7305ae6fe1b3f214247df05893756
Petre Diana <petre.diana98@yahoo.ro>
2020-11-17 19:48:18 +0200

    seminar 7

---
+++ seminar_7/TestReflection/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TestReflection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.FR = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FR,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // FR
+            // 
+            this.FR.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.FR.Image = ((System.Drawing.Image)(resources.GetObject("FR.Image")));
+            this.FR.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.FR.Name = "FR";
+            this.FR.Size = new System.Drawing.Size(26, 22);
+            this.FR.Text = "EN";
+            this.FR.Click += new System.EventHandler(this.EN_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(27, 22);
+            this.toolStripButton2.Text = "RO";
+            this.toolStripButton2.Click += new System.EventHandler(this.RO_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(24, 22);
+            this.toolStripButton3.Text = "FR";
+            this.toolStripButton3.Click += new System.EventHandler(this.FR_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(184, 293);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(265, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(432, 190);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton FR;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ seminar_7/TestReflection/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestReflection
+{
+    public partial class Form1 : Form
+    {
+        private Assembly _assembly;
+        private string _lang = "EN";
+        private string _button1Name = "Button_OK";
+        private string _button2Name = "Button_Cancel";
+        private string _button3Name = "Button_Close";
+        private Bitmap _img;
+        private ResourceManager _resourceManager;
+
+        public Form1()
+        {
+            _assembly = Assembly.Load("TestReflectionClassLibrary");
+            _resourceManager = new ResourceManager("TestReflectionClassLibrary.Resource1", _assembly);
+            InitializeComponent();
+            IncarcaTextControale();
+        }
+
+        private void IncarcaTextControale()
+        {
+            button1.Text = _resourceManager.GetString(_button1Name + "_" + _lang);
+            button2.Text = _resourceManager.GetString(_button2Name + "_" + _lang);
+            button3.Text = _resourceManager.GetString(_button3Name + "_" + _lang);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var types = _assembly.GetTypes();
+            foreach (var type in types)
+            {
+                var typeInfo = type.GetTypeInfo();
+                textBox1.Text += "Tipul: " + type.FullName + " are " + typeInfo.DeclaredProperties.Count()
+                                    + " proprietati, " + typeInfo.DeclaredConstructors.Count()
+                                    + " constructori, " + typeInfo.DeclaredMethods.Count()
+                                    + " metode, " + typeInfo.DeclaredEvents.Count()
+                                    + " evenimente \r\n\n";
+
+                MemberInfo[] memberInfos = type.GetMembers();
+                foreach (var memberInfo in memberInfos)
+                {
+                    textBox1.Text += memberInfo.MemberType + " " + memberInfo.Name + "\r\n\n";
+                }
+
+                if (type.Name == "Resource1")
+                {
+                    _img = (Bitmap)_resourceManager.GetObject("IMG1");
+                    panel1.BackgroundImage = _img;
+                }
+            }
+        }
+
+        private void EN_Click(object sender, EventArgs e)
+        {
+            _lang = "EN";
+            IncarcaTextControale();
+        }
+
+        private void RO_Click(object sender, EventArgs e)
+        {
+            _lang = "RO";
+            IncarcaTextControale();
+        }
+
+        private void FR_Click(object sender, EventArgs e)
+        {
+            _lang = "FR";
+            IncarcaTextControale();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Type type = _assembly.GetType("TestReflectionClassLibrary.Produs");
+            
+            Type[] stringArgumentTypes = new Type[] { };
+            ConstructorInfo stringConstructor = type.GetConstructor(stringArgumentTypes);
+            object produs = stringConstructor.Invoke(new object[] { });
+
+
+            MethodInfo method = type.GetMethod("AfiseazaProdus");
+            string rezultat = (string)method.Invoke(produs, new object[] { });
+
+            MessageBox.Show(rezultat);
+        }
+    }
+}
