270794c47fa6d81f42d2b8fa1a2f9130c5f7afe4
Petre Diana <petre.diana98@yahoo.ro>
2021-02-06 20:33:16 +0200

    Add files via upload

---
+++ Reflection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void modificaValori(object obiect, Dictionary<string,object> dictionar)
+        {
+            Type tipObiect = obiect.GetType();
+            foreach(FieldInfo camp in tipObiect.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                object value;
+                dictionar.TryGetValue(camp.Name, out value);
+                camp.SetValue(obiect, value);
+            }
+        }
+        static void showValues(object obiect)
+        {
+            Type tipObiect = obiect.GetType();
+            foreach (FieldInfo camp in tipObiect.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                Console.WriteLine("Denumire camp: {0} | Tip camp: {1} | Valoare camp: {2}", camp.Name, camp.FieldType, camp.GetValue(obiect));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> dictionar = new Dictionary<string, object>();
+            dictionar.Add("idPrajitura", 1);
+            dictionar.Add("aroma", "cacao");
+            dictionar.Add("cantitate", 10);
+            dictionar.Add("pret", 35.50);
+
+            Prajitura p = (Prajitura)Activator.CreateInstance(typeof(Prajitura), 3);
+            showValues(p);
+            modificaValori(p, dictionar);
+            showValues(p);
+        }
+    }
+}
